039da80905bd240b78b479fdc0773c649c97ac22
JohnyDux <126690778+JohnyDux@users.noreply.github.com>
2023-11-28 17:15:04 +0000

    Timer

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -10,11 +10,30 @@ public class Score : MonoBehaviour
     public int scorePoints;
     public TextMeshProUGUI missText;
     public DestroyerCheckers check;
+
+    [Header("Time Left in Seconds")]
+    public float timeLeft = 300;
+    
+    public TextMeshProUGUI timerText;
     public void ScoreUpdate(int score)
     {
         scorePoints += score;
         scoreText.text = scorePoints.ToString("0") + " Hit";
 
         missText.text = check.numberOfMisses.ToString("0") + " Missed";
+    }
+
+    public void Update()
+    {
+        if (timeLeft > 0)
+        {
+            timeLeft -= Time.deltaTime * 2;
+
+            string minituesLeft = Mathf.FloorToInt(timeLeft / 60).ToString();
+            string seconds = (timeLeft % 60).ToString("F0");
+            seconds = seconds.Length == 1 ? seconds = "0" + seconds : seconds;
+
+            timerText.text = minituesLeft + ":" + seconds;
+        }
     }
 }
