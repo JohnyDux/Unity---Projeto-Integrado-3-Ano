4594392c8c2a360b7e86755c8b940c11256baa5a
JohnyDux <126690778+JohnyDux@users.noreply.github.com>
2024-01-05 01:49:38 +0000

    UI Maze Fixed

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -5,10 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
+    public SettingsMenu settingsRef;
     public void LoadScene(string SceneName)
     {
         SceneManager.LoadScene(SceneName);
         Time.timeScale = 1f;
+        settingsRef.SetResolution(settingsRef.current_resolution);
+        settingsRef.SetFullscreen(settingsRef.fullscreen);
+        settingsRef.SetQuality(settingsRef.current_quality);
+        settingsRef.SetMasterVolume(settingsRef.current_masterVolume);
+        settingsRef.SetMusicVolume(settingsRef.current_musicVolume);
+        settingsRef.SetSoundFXVolume(settingsRef.current_soundFXVolume);
     }
 
     public void ExitGame()
